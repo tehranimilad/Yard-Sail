d6e96359b782f4fbf1573e60c3e211ed1fa572c1
desireevharvey <desireevharvey@gmail.com>
2022-12-20 13:49:55 -0500

    staged change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 
 
