83ee829ed7b12adb050f2fd56312defc65fac922
desireevharvey <desireevharvey@gmail.com>
2022-12-20 13:05:51 -0500

    debugged index.html ready to push

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
