10a2930ce8e25e3ef9678a38a181c83f8676ddd8
tehranimilad <tehranimilad92@gmail.com>
2022-12-20 10:05:39 -0800

    Debugged index html issue, updating code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+//Packages
+// import { useState } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+
+//Pages
+
+import Home from './pages/Home';
+
+
 
 function App() {
   return (
     <div className="App">
-    
+    <Home />
     </div>
   );
 }
