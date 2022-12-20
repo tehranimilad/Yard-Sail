140e6e34fddbc88df51fd7242bcb56a4405f2417
tschmiedl <tschmiedl@me.com>
2022-12-20 10:05:31 -0800

    index.html fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    < App />
+    <App />
   </Router>
-)
-  
+);
 
 
 
