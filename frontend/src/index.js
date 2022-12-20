cca8169e6fd7ee57bd2b61d58bc59823e8b09c6f
tehranimilad <tehranimilad92@gmail.com>
2022-12-20 10:23:11 -0800

    user controller created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,14 +6,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    < App />
   </Router>
-);
+)
+  
 
 
 
