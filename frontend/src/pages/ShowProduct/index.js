df84cb2b431cf49b3277ce1397650c28f8e8c347
desireevharvey <desireevharvey@gmail.com>
2022-12-21 18:04:35 -0500

    added code to show product index

--- frontend/src/pages/ShowProduct/index.js
+++ frontend/src/pages/ShowProduct/index.js
@@ -1,9 +1,19 @@
-const ShowProduct = () => {
 
-    
+export default function ShowProduct(props) {
 
     return(
-        <h1>This is our Show Page</h1>
+        <div className="card">
+        <div className="card-image">
+            <img src={props.info.imageUrl} alt={props.info.name} className='product-image' />
+            <span className="card-title">{props.info.name}</span>
+        </div>
+
+        <div className="card-content">
+            {props.isLoggedIn && <p>Name: {props.info.name}</p>}
+            {props.isLoggedIn && <p>Price: {props.info.price}</p>}
+        </div>
+    </div>
     )
 }
-export default ShowProduct
+
+
