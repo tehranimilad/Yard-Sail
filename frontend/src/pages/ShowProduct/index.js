58d9eca252fc949cdd6c6eaf69f3dd7694b0d22f
desireevharvey <desireevharvey@gmail.com>
2022-12-22 13:46:46 -0500

    show product page is now functioning properly

--- frontend/src/pages/ShowProduct/index.js
+++ frontend/src/pages/ShowProduct/index.js
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import { useParams } from "react-router-dom"
 import { showOneProduct } from "../../utils/api"
+import Card from 'react-bootstrap/Card';
+
+
 
 const ShowProduct = () => {
 
@@ -12,13 +15,17 @@ const ShowProduct = () => {
   
 
     return(
-        <>
-        <div>
-        <h1>{showProductData.title}</h1>
-        <p>{showProductData.description}</p>
-        </div>
-
-        </>
-    )
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={showProductData.image} />
+        <Card.Body>
+          <Card.Title>{showProductData.title}</Card.Title>
+          <Card.Text>
+          <p>{showProductData.description}</p>
+          <p>{showProductData.location}</p>
+          <p>{showProductData.price}</p>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
 }
 export default ShowProduct
