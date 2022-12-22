1f608dda48a6aef51b8a4a9cec1f33d23ad1a082
tschmiedl <tschmiedl@me.com>
2022-12-21 17:11:38 -0800

    ShowProduct functioning

--- frontend/src/components/ProductCard/index.js
+++ frontend/src/components/ProductCard/index.js
@@ -1,14 +1,21 @@
-
+import { Link } from "react-router-dom"
 const ProductCard = ({product}) => {
+    
    
     return(
         <div className="col s12 m6 l3">
+            
                 <div className="card" style={{width: '18rem'}}>
+                <Link to={"/product/" + product._id}>
                   <img className="card-img-top" src={product.image} alt={product.title} />
                   <div className="card-body">
                     <h5 className="card-title">{product.title}</h5>
+                    </div>
+                    </Link>
+                    <div className="card-body">
                     <p className="card-text">{product.description}</p>
                     <h6 className="card-text">${product.price}</h6>
+                    <h1>{product._id}</h1>
                   </div>
                 </div>
         </div>
--- frontend/src/pages/ShowProduct/index.js
+++ frontend/src/pages/ShowProduct/index.js
@@ -1,9 +1,24 @@
+import { useState } from "react"
+import { useParams } from "react-router-dom"
+import { showOneProduct } from "../../utils/api"
+
 const ShowProduct = () => {
 
+    const [showProductData, setShowProductData] = useState({})
+
+    const {id} = useParams()
+    showOneProduct(id).then(data => {setShowProductData(data)})
     
+  
 
     return(
-        <h1>This is our Show Page</h1>
+        <>
+        <div>
+        <h1>{showProductData.title}</h1>
+        <p>{showProductData.description}</p>
+        </div>
+
+        </>
     )
 }
 export default ShowProduct
