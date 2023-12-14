52b3d0f6dfa009c3941d69ea9741fed0155c2582
Milad Tehrani <114964227+tehranimilad@users.noreply.github.com>
2023-12-14 15:55:53 -0500

    Update index.js

--- frontend/src/components/ProductCard/index.js
+++ frontend/src/components/ProductCard/index.js
@@ -8,14 +8,13 @@ const ProductCard = ({product}) => {
         <div className="col s12 m6 l3">
                 <div className="card mx-auto" style={{width: '18rem'}}>
                 <Link to={"/" + product._id}>
-                  <img className="card-img-top" src={product.image} alt={product.title} />
+                  <img className="card-img-top" src={product.title}/>
                   <div className="card-body">
                     <h5 className="card-title">{product.title}</h5>
                     </div>
                     </Link>
                     <div className="card-body">
                     <p className="card-text">{product.description}</p>
-                    <h6 className="card-text">${product.price}</h6>
                     
                   </div>
                 </div>
