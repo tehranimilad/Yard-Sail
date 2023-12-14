2fd9ebe2e8d6861d095a6416efa96b1b9eb37b73
Milad Tehrani <114964227+tehranimilad@users.noreply.github.com>
2023-12-14 15:43:29 -0500

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -2,14 +2,8 @@ const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    description: {type: String, required: true},
-    image: {type: String},
-    location: {type: String},
-    price: {type: Number},
-    contact: {type: String},
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+    description: {type: String, required: true}
+})
     }
 })
 
